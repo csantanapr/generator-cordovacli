f85fbdf22a756988e2dd92ad26d8a568624a609f
Carlos Santana <csantana23@gmail.com>
2013-11-26 00:01:02 -0500

    update test case

--- test/test-creation.js
+++ test/test-creation.js
@@ -27,7 +27,9 @@ describe('cordovacli generator', function () {
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': true
+      'appName': 'dude',
+      'platforms': ['android'],
+      'plugins' : ['device']
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
