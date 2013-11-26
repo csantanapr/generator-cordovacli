fdc62816ba94298f6ec1c7ebcfbea5783a2978c0
Carlos Santana <csantana23@gmail.com>
2013-11-25 23:43:31 -0500

    initial commit

---
+++ app/index.js
@@ -0,0 +1,205 @@
+/*global require, module, __dirname, console*/
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var os     = require('os');
+
+
+var CordovacliGenerator,
+    getPlatformChoices,
+    getPluginsChoices;
+CordovacliGenerator = module.exports = function CordovacliGenerator(args, options) {
+    yeoman.generators.Base.apply(this, arguments);
+
+    this.on('end', function () {
+        this.installDependencies({ skipInstall: options['skip-install'] });
+    });
+
+    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(CordovacliGenerator, yeoman.generators.Base);
+
+CordovacliGenerator.prototype.askFor = function askFor() {
+    var cb,
+        prompts;
+    cb = this.async();
+
+    // have Yeoman greet the user.
+    console.log(this.yeoman);
+
+    prompts = [{
+        'name': 'appName',
+        'message': 'What do you want to name your App?',
+        'default': 'dude'
+    },
+    {
+        type: 'checkbox',
+        name: 'platforms',
+        message: 'What platforms would you like to add support for?',
+        choices: getPlatformChoices()
+    },
+    {
+        type: 'checkbox',
+        name: 'plugins',
+        message: 'What plugins would you like to add support for?',
+        choices: getPluginsChoices()
+    }];
+
+    this.prompt(prompts, function (props) {
+        for (var key in props) {
+            this[key] = props[key];
+        }
+
+        this.platforms = '[\'' + this.platforms.toString().split(',').join('\', \'') + '\']';
+        this.plugins = '[\'' + this.plugins.toString().split(',').join('\', \'') + '\']';
+
+
+        cb();
+    }.bind(this));
+};
+
+CordovacliGenerator.prototype.app = function app() {
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+};
+
+CordovacliGenerator.prototype.projectfiles = function projectfiles() {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+    this.template('Gruntfile.js', 'Gruntfile.js');
+};
+
+getPlatformChoices = function () {
+    var choices = [];
+
+    choices.push({
+        name: 'Android',
+        value: 'android',
+        checked: true
+    });
+    if (os.platform() === 'darwin') {
+        choices.push({
+            name: 'iOS',
+            value: 'ios',
+            checked: true
+        });
+    }
+    choices.push({
+        name: 'Blackberry 10',
+        value: 'blackberry10',
+        checked: true
+    });
+    if (os.platform() === 'win32') {
+        choices.push({
+            name: 'Windows Phone 8',
+            value: 'wp8',
+            checked: true
+        });
+
+        choices.push({
+            name: 'Windows 8',
+            value: 'windows8',
+            checked: true
+        });
+    }
+    choices.push({
+        name: 'Firefox OS',
+        value: 'firefoxos',
+        checked: false
+    });
+    return choices;
+};
+getPluginsChoices = function () {
+    var choices;
+
+    choices = [{
+        name: 'Device',
+        value: 'device',
+        checked: true
+    },
+    {
+        name: 'Dialogs',
+        value: 'dialogs',
+        checked: false
+    },
+    {
+        name: 'Battery Status',
+        value: 'battery-status',
+        checked: false
+    },
+    {
+        name: 'Camera',
+        value: 'camera',
+        checked: false
+    },
+    {
+        name: 'Console',
+        value: 'console',
+        checked: false
+    },
+    {
+        name: 'Contacts',
+        value: 'contacts',
+        checked: false
+    },
+    {
+        name: 'Device Motion',
+        value: 'device-motion',
+        checked: false
+    },
+    {
+        name: 'Device Orientation',
+        value: 'device-orientation',
+        checked: false
+    },
+    {
+        name: 'File',
+        value: 'file-transfer',
+        checked: false
+    },
+    {
+        name: 'Geolocation',
+        value: 'geolacation',
+        checked: false
+    },
+    {
+        name: 'Globalization',
+        value: 'globalization',
+        checked: false
+    },
+    {
+        name: 'In App Browser',
+        value: 'inappbrowser',
+        checked: false
+    },
+    {
+        name: 'Media',
+        value: 'media',
+        checked: false
+    },
+    {
+        name: 'Media Capture',
+        value: 'media-capture',
+        checked: false
+    },
+    {
+        name: 'Network Information',
+        value: 'network-information',
+        checked: false
+    },
+    {
+        name: 'Splashscreen',
+        value: 'splashscreen',
+        checked: false
+    },
+    {
+        name: 'Vibration',
+        value: 'vibration',
+        checked: false
+    }];
+
+
+    return choices;
+};
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,103 @@
+/*global module, require */
+/*jshint camelcase: false */
+
+module.exports = function (grunt) {
+    'use strict';
+
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    // configurable paths
+    var yeomanConfig = {
+            app: 'app',
+            dist: 'dist'
+        };
+    // Project configuration.
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        jshint: {
+            gruntfile: {
+                src: 'Gruntfile.js'
+            }
+        },
+        watch: {
+            cordova: {
+                files: '<%%= yeoman.app %>/www/**',
+                tasks: ['build', 'emulate']
+            }
+        },
+        cordovacli: {
+            options: {
+                path: '<%%= yeoman.app %>'
+            },
+            cordova: {
+                options: {
+                    command: ['create', 'platform', 'plugin', 'build'],
+                    platforms: <%= platforms %>,
+                    plugins:  <%= plugins %>,
+                    id: 'io.cordova.hellocordova',
+                    name: '<%= _.slugify(appName) %>'
+                }
+            },
+            build: {
+                options: {
+                    command: 'build'
+                }
+            },
+            emulate: {
+                options: {
+                    command: 'emulate'
+                }
+            },
+            prepare: {
+                options: {
+                    command: 'prepare'
+                }
+            },
+            compile: {
+                options: {
+                    command: 'compile'
+                }
+            }
+        },
+        clean: {
+            cordova: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%%= yeoman.app %>'
+                    ]
+                }]
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('lint',    ['jshint']);
+    grunt.registerTask('server',  ['build', 'emulate', 'watch:cordova']);
+    grunt.registerTask('emulate', ['cordovacli:emulate']);
+    grunt.registerTask('demo',    ['build', 'emulate']);
+
+    grunt.registerTask('build',   ['lint', 'cordovacli:build']);
+
+    grunt.task.registerTask('build', 'Builds a Cordova App', function () {
+        var check;
+
+        check = (yeomanConfig.app + '/.cordova/config.json');
+
+        if (grunt.file.exists(check)) {
+            grunt.log.writeln(check + ' exists, only do build');
+            grunt.task.run(['cordovacli:build']);
+        } else {
+            grunt.log.writeln(check + ' does not exists, creating app and building');
+            grunt.task.run(['cordovacli:cordova', 'cordovacli:build']);
+        }
+
+    });
+
+    grunt.registerTask('prepare', ['cordovacli:prepare']);
+    grunt.registerTask('compile', ['cordovacli:compile']);
+
+};
